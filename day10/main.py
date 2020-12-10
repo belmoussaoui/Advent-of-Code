4c36b4ade25d5974c6cb2d208d6f6c7650867d74
belmoussaoui <emb-12@outlook.com>
2020-12-10 23:06:09 +0100

    Add day10/part2

--- day10/main.py
+++ day10/main.py
@@ -7,8 +7,7 @@ def part1():
     while (count <= len(output_voltage)):
         count += 1
         for i in range(1, adapter_rated + 1):
-            is_find = find_adapter(joltage + i, output_voltage)
-            if (is_find):
+            if (find_adapter(joltage + i, output_voltage)):
                 joltage += i
                 add_difference(differences, i)
                 break
@@ -36,11 +35,52 @@ def parser():
         data.append(int(line))
     return data
 
+
 def part2():
-    data = parser()
-    pass
+    output_voltage = parser()
+    adapter_rated = 3
+    res = 1
+    output_cut = cut_output(output_voltage, adapter_rated)
+    for output in output_cut:
+        res *= recursive(output, adapter_rated, output[0])
+    print(res)
+
+def cut_output(data, adapter_rated):
+    """
+        Need to cut the file into subproblems for performance reasons.
+    """
+    output = []
+    # don't forget to add the 0!
+    data.append(0)
+    data.sort()
+    start = 0
+    for end in range(len(data)):
+        count = 0
+        for i in range(1, 4):
+            if (find_adapter(data[end] + i, data)):
+                count += 1
+        # data[j + 1] is an obligatory adapter and so a cut
+        if count == 1:
+            cut_index = end + 1
+            # has more than 1 possibility of combination
+            if cut_index - start >= 2:
+                output.append(data[start:cut_index + 1])
+            start = cut_index
+    return output
+
+def recursive(output_voltage, adapter_rated, joltage, res=1):
+    first_find = True
+    for i in range(1, adapter_rated + 1):
+        is_find = find_adapter(joltage + i, output_voltage)
+        if (is_find):
+            if first_find:
+                first_find = False
+            else:
+                res += 1
+            res = recursive(output_voltage, adapter_rated, joltage + i, res)
+    return res
 
 
 if __name__ == '__main__':
     part1()
-    #part2()
+    part2()
