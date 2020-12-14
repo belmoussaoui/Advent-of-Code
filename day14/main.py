6e711eb55944c8a3058d64e7a583ecca50191854
belmoussaoui <emb-12@outlook.com>
2020-12-14 21:14:16 +0100

    Add day14/part2

--- day14/main.py
+++ day14/main.py
@@ -1,4 +1,5 @@
 import math
+import itertools
 
 def parser():
     file = open("data.txt", "r")
@@ -38,9 +39,37 @@ def part1():
 
 
 def part2():
-    pass
+    instructions = parser()
+    res = {}
+    length = 36
+    mask = ""
+    res = {}
+    for instruction in instructions:
+        if instruction.get("mask", False):
+            mask = instruction["mask"]
+            continue
+        add = bin(instruction["add"])[2:].zfill(length)
+        value = instruction["value"]
+        result = ""
+        for i in range(length):
+            if mask[i] == "X":
+                result += "X"
+            elif mask[i] == "1":
+                result += "1"
+            else:
+                result += add[i]
+        combination(result, value, res)
+    print(sum(res.values()))
 
+def combination(result, value, res):
+    index = [i for i in range(len(result)) if result[i] == "X"]
+    combs = list(map(list, itertools.product([0, 1], repeat=len(index))))
+    for comb in combs:
+        current = result
+        for i in range(len(index)):
+            current = current.replace("X", str(comb[i]), 1)
+        res[int(current, 2)] = value
 
 if __name__ == "__main__":
     part1()
-    #part2()
+    part2()
