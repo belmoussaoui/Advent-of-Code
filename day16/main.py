3939edc89ff8cdc3531a47d89deadcde395aa15c
belmoussaoui <emb-12@outlook.com>
2020-12-18 07:20:33 +0100

    Add day16/part2

--- day16/main.py
+++ day16/main.py
@@ -14,31 +14,65 @@ def parser_part1():
                 res = re.search(": (.*) or (.*)$", line)
                 cond1 = res.groups()[0]
                 cond2 = res.groups()[1]
-                fields.append(cond1.split("-"))
-                fields.append(cond2.split("-"))
+                fields.append([cond1.split("-"), cond2.split("-")])
             elif part_id == 2:
-                pass
+                self_ticket = line.strip().split(',')
             else:
-                tickets += line.strip().split(',')
-    return fields, tickets[1:]
+                tickets.append(line.strip().split(','))
+    return fields, self_ticket, tickets[1:]
 
 def part1():
-    fields, tickets = parser_part1()
+    fields, self_ticket, list_tickets = parser_part1()
     invalid = []
-    for ticket in tickets:
-        is_invalid = True
-        for cond in fields:
-            if int(cond[0]) <= int(ticket) <= int(cond[1]):
-                is_invalid = False
-                break
-        if is_invalid:
-            invalid.append(int(ticket))
+    for tickets in list_tickets:
+        for ticket in tickets:
+            is_invalid = True
+            for cond in fields:
+                if int(cond[0][0]) <= int(ticket) <= int(cond[0][1]):
+                    is_invalid = False
+                    break
+                if int(cond[1][0]) <= int(ticket) <= int(cond[1][1]):
+                    is_invalid = False
+                    break
+            if is_invalid:
+                invalid.append(int(ticket))
     print(sum(invalid))
+    return invalid
 
 
-def part2():
-    pass
+def part2(invalid):
+    fields, self_ticket, list_tickets = parser_part1()
+    valid = [[i for i in range(len(self_ticket))] for j in range(len(self_ticket))]
+    for tickets in list_tickets:
+        ticket_id = 0
+        for ticket in tickets:
+            field_id = 0
+            for cond in fields:
+                if not int(ticket) in invalid:
+                    if not (int(cond[0][0]) <= int(ticket) <= int(cond[0][1])):
+                        if  not (int(cond[1][0]) <= int(ticket) <= int(cond[1][1])):
+                            if field_id in valid[ticket_id]:
+                                valid[ticket_id].remove(field_id)
+                field_id += 1
+            ticket_id += 1
+    queue = []
+    for elem in valid:
+        if len(elem) == 1:
+            queue.append(elem[0])
+    while len(queue) > 0:
+        field_id = queue.pop()
+        for elem in valid:
+            if field_id in elem:
+                if (len(elem) > 1):
+                    elem.remove(field_id)
+                    if (len(elem) == 1):
+                        queue.append(elem[0])
+    res = 1
+    for x in range(len(valid)):
+        if (valid[x][0] < 6):
+            res *= int(self_ticket[x])
+    print(res)
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    invalid = part1()
+    part2(invalid)
